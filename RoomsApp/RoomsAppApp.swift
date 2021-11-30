300b20e0967f36d13690fc4bf23e3709cf742342
Mohammad Azam <azamsharp@gmail.com>
2021-11-30 17:57:38 -0600

    Initial Commit

---
+++ RoomsApp/RoomsAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  RoomsAppApp.swift
+//  RoomsApp
+//
+//  Created by Mohammad Azam on 11/30/21.
+//
+
+import SwiftUI
+
+@main
+struct RoomsAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
